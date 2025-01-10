39aa5a14b033926187144153f7f00967a31fab67
David Klemme <david.klemme@roedl.com>
2025-01-10 14:08:57 +0100

    fix empty var

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Inter } from 'next/font/google'
 
 
 import "./globals.css";
-
-// const inter = Inter({ 
-//   subsets: ['latin'],
-//   weight: ['400', '600', '700', '800'],
-//   variable: '--font-inter',
-// })
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
